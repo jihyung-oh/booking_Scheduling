b9395f6ad90965a5cd3649065c91109e93ea6fe3
jihyung <lifehappy@naver.com>
2025-08-01 13:02:59 +0900

    [feature]step.7

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -1,6 +1,6 @@
 import pytest
 
-
+from pytest_mock import MockerFixture
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
@@ -17,6 +17,13 @@ CAPACITY_PER_HOUR = 3
 @pytest.fixture
 def booking_scheduler():
     return BookingScheduler(CAPACITY_PER_HOUR)
+
+@pytest.fixture
+def booking_scheduler_with_sms_mock():
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+    testable_sms_sender = TestableSmsSender()
+    booking_scheduler.set_sms_sender(testable_sms_sender)
+    return booking_scheduler, testable_sms_sender
 
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(booking_scheduler):
     schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
@@ -53,15 +60,14 @@ def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capac
     assert booking_scheduler.has_schedule(schedule)
     assert booking_scheduler.has_schedule(new_schedule)
 
-def test_예약완료시_SMS는_무조건_발송(booking_scheduler):
+def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock):
     # arrange
-    testable_sms_sender = TestableSmsSender()
-    booking_scheduler.set_sms_sender(testable_sms_sender)
+    booking_scheduler,sms_mock = booking_scheduler_with_sms_mock
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
     # act
     booking_scheduler.add_schedule(schedule)
     # assert
-    assert testable_sms_sender.send_called
+    assert sms_mock.send_called
 
 
 def test_이메일이_없는_경우에는_이메일_미발송():
